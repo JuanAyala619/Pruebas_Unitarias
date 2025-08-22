080f7131cb4a8d1739ddc6fb559b7b70308824ea
GabrielVillao <137122179+GabrielVillao@users.noreply.github.com>
2025-08-21 23:18:25 -0500

    Delete Tarea-Aspectos/demo directory

--- Tarea-Aspectos/demo/src/main/java/com/example/Adapter/ImpresoraNueva.java
+++
@@ -1,5 +0,0 @@
-package com.example.Adapter;
-
-public interface ImpresoraNueva {
-    void imprimirDocumento(String texto);
-}
--- Tarea-Aspectos/demo/src/main/java/com/example/Adapter/ImpresoraVieja.java
+++
@@ -1,7 +0,0 @@
-package com.example.Adapter;
-
-public class ImpresoraVieja { //Clase existente incompatible
-    public void imprimir(String texto){
-        System.out.println("Impresora vieja"+texto);
-    }
-}
